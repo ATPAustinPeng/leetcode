40114f67be0a7465495b991258bc1d7e661b89b9
Austin Peng <austinpeng2002@gmail.com>
2021-12-25 18:43:07 -0800

    solve Problem 238

---
+++ Problem238.java
@@ -0,0 +1,43 @@
+/*
+	Problem 238. Product of Array Except Self
+
+	Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+	The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+	You must write an algorithm that runs in O(n) time and without using the division operation.
+	
+	Example 1:
+	Input: nums = [1,2,3,4]
+	Output: [24,12,8,6]
+
+	Example 2:
+	Input: nums = [-1,1,0,-3,3]
+	Output: [0,0,9,0,0]
+
+	Constraints:
+	2 <= nums.length <= 105
+	-30 <= nums[i] <= 30
+	The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+*/
+
+public class Problem238 {
+	public int[] productExceptSelf(int[] nums) {
+		// first pass: find trailing products (left to right)
+		// second pass: find trailing products (right to left)
+
+		int[] tempArr = new int[nums.length];
+
+		int temp = 1;
+		for (int i = 0; i < nums.length; i++) {
+			tempArr[i] = temp;
+			temp *= nums[i];
+		}
+
+		temp = 1;
+		for (int i = nums.length - 1; i >= 0; i--) {
+			tempArr[i] *= temp;
+			temp *= nums[i];
+		}
+
+		return tempArr;
+	}
+}
