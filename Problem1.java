8a7befcb3358fdbd1dc3087a6b9a094cbbf2e952
Austin Peng <austinpeng2002@gmail.com>
2021-12-25 18:42:14 -0800

    modify solution to Problem1

--- Problem1.java
+++ Problem1.java
@@ -26,16 +26,17 @@
 */
 
 public class Problem1 {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (hmap.containsKey(complement)) {
-                return new int[] {i, hmap.get(complement)};
-            }
-            hmap.put(nums[i], i);
-        }
-        return new int[] {-1, -1};
-    }
+	public int[] twoSum(int[] nums, int target) {
+		// hashmap to track complement of previous numbers + index of original number
+		HashMap<Integer, Integer> hmap = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			if (!hmap.containsKey(nums[i])) {
+				hmap.put(target - nums[i], i);
+			} else {
+				return new int[] { i, hmap.get(nums[i]) };
+			}
+		}
+		return null;
+	}
 }
