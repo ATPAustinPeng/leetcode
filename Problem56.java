2e872779963257541654f9bee1dc90facd22f4f8
ATPAustinPeng <austinpeng2002@gmail.com>
2022-02-28 23:57:37 -0500

    comment solution for Problem 56

--- Problem56.java
+++ Problem56.java
@@ -19,8 +19,20 @@
 */
 public class Problem 56 {
     public int[][] merge(int[][] intervals) {
+        // sort the array by x[0] then by y[0] if the x[0]'s are equal
         Arrays.sort(intervals, (x, y) -> Integer.compare(x[0], y[0]));
+
         LinkedList<int[]> solution = new LinkedList<>();
+
+        // loop through sorted intervals
+        // if solution set is empty, add the first interval we see
+        // OR add the first interval where the first value > solution.getLast()'s second value
+            // ex. solution = {[1, 2], [3, 15]}, arr = [29, 50]
+            // because 29 > 15, we want to add the interval because no merging is needed
+        // otherwise
+            // if the interval's second value > solution.getLast()[1], update solution.getLast()[1] with the max(interval[1], solution.getLast()[1])
+            // ex. solution = {[1, 2], [3, 15]}, arr = [10, 30]
+            // because 30 > 15, we want to merge interval to [3, 30]
         for (int[] arr : intervals) {
             if (solution.isEmpty() || solution.getLast()[1] < arr[0]) {
                 solution.add(arr);
